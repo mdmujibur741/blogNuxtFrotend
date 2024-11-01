9b215ed1d301a10412b9de5aa7b0d7c0b539b2d8
Mujan9856 <mdmujibur741@gmail.com>
2024-11-01 20:23:34 +0600

    .

--- components/frontend/TopMenu.vue
+++ components/frontend/TopMenu.vue
@@ -34,9 +34,10 @@
   
       <!-- Mobile Menu -->
       <div v-if="menuOpen" class="md:hidden bg-white shadow-lg rounded-lg p-4 space-y-4">
-        <nuxt-link to="/" class="text-gray-700 hover:text-gray-900">Home</nuxt-link> <br>
-            <nuxt-link to="/blogs" class="text-gray-700 hover:text-gray-900">Blogs</nuxt-link>
+        <nuxt-link to="/" class="text-gray-700 hover:text-gray-900">Home</nuxt-link> 
+          
         <a href="#" class="block text-gray-700 hover:text-gray-900">About</a>
+        <nuxt-link to="/blogs" class="text-gray-700 hover:text-gray-900">Blogs</nuxt-link>
         <a href="#" class="block text-gray-700 hover:text-gray-900">Contact</a>
       </div>
     </div>
